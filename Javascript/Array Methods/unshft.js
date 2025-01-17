2557e2063301856a68f8666444ca9e645949d144
ridhi-priya <ridhi7681309@gmail.com>
2025-01-17 23:52:55 +0530

    added unshift array method

---
+++ Javascript/Array Methods/unshft.js
@@ -0,0 +1,10 @@
+// The unshift() method in JavaScript is used to add one or more elements to the beginning of an array.
+// It modifies the original array and returns the new length of the array.
+// array.unshift(element1, element2, ..., elementN)
+
+var fruits = ["mango", "kiwi", "pears", "apple", "Banana", "papaya"];
+console.log(fruits.length);
+fruits.unshift("Grapes", "pine apple");
+
+console.log(fruits.length);
+console.log(fruits);
---
+++ Javascript/spread.js
@@ -0,0 +1,3 @@
+let ori = [1, 2, 3];
+let copy = [...ori];
+console.log(copy);
