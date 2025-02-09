0c3f26ddf0fe89f345be777877a592fe3601f4c0
ridhi-priya <ridhi7681309@gmail.com>
2025-02-09 14:09:44 +0530

    added looping.js

--- Javascript/Array Methods/looping.js
+++ Javascript/Array Methods/looping.js
@@ -1,4 +1,14 @@
 let fruits = ["mango", "kiwi", "pears", "apple"];
+
+// Using a for loop
 for (let i = 0; i < fruits.length; i++) {
   console.log(fruits[i]);
+}
+
+// Using a while loop (Corrected)
+let j = 0;
+while (j < fruits.length) {
+  // Fix: Use 'j < fruits.length' instead of 'fruits.length > 0'
+  console.log(fruits[j]);
+  j++;
 }
