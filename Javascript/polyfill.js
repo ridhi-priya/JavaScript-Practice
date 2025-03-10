4f7242225129487c8a9f01079d07f7e5761b28b6
Ridhi Priya <ridhi7681309@gmail.com>
2025-03-11 01:16:37 +0530

    polyfills

--- Javascript/polyfill.js
+++ Javascript/polyfill.js
@@ -1,1 +1,22 @@
 // Creating polyfill of array methods
+// map, filter and reduce polyfills
+// map polyfill
+
+const arr = [1, 2, 3, 4, 5];
+
+let a = arr.map((i) => {
+  return i * 2;
+});
+console.log(arr);
+console.log(a);
+
+// filter polyfill
+let b = arr.filter((i) => {
+  return i > 2;
+});
+console.log(b);
+// reduce polyfill
+let c = arr.reduce((acc, i) => {
+  return acc + i;
+}, 0);
+console.log(c);
