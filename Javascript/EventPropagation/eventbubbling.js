e5e21af3fd643d5367b4cffa3342d586ecaef0a7
Ridhi Priya <ridhi7681309@gmail.com>
2025-03-13 03:32:19 +0530

    added event bubbling

---
+++ Javascript/EventPropagation/eventbubbling.js
@@ -0,0 +1,13 @@
+const div = document.querySelector("div");
+const form = document.querySelector("form");
+const button = document.querySelector("button");
+
+div.addEventListener("click", function () {
+  alert("div");
+});
+form.addEventListener("click", function () {
+  alert("form");
+});
+button.addEventListener("click", function () {
+  alert("button");
+});
