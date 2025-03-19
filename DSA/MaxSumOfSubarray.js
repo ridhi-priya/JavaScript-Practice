02fdabb13e438e09b030e8d04d2746add281b709
Ridhi Priya <ridhi7681309@gmail.com>
2025-03-19 14:54:11 +0530

    added slidingwindow MaxSumOfSuArray

--- DSA/MaxSumOfSubarray.js
+++ DSA/MaxSumOfSubarray.js
@@ -13,7 +13,22 @@ function maxSumBruteForce(arr, k) {
   }
   return max;
 }
+function maxSumSlidingWindow(arr, k) {
+  let sum = 0;
+  for (let i = 0; i < k; i++) {
+    sum += arr[i];
+  }
+  let max = sum;
+  for (let i = 1; i <= arr.length - k; i++) {
+    sum = sum - arr[i - 1] + arr[i + k - 1];
+    if (sum > max) {
+      max = sum;
+    }
+  }
+  return max;
+}
 
 let arr = [3, 8, 2, 5, 7, 6, 12];
 let k = 4;
 console.log(maxSumBruteForce(arr, k));
+console.log(maxSumSlidingWindow(arr, k));
